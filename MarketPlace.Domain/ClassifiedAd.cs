a0d4cb496c348541dae2bf56c0e89af3877f57d8
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-20 13:04:51 +0530

    * ClassifiedAd.cs: Adds a class for ClassifiedAd.
    * UnitTest1.cs:
    * Class1.cs:
    * MarketPlace.Domain.csproj: Removes unwanted classes.

---
+++ MarketPlace.Domain/ClassifiedAd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MarketPlace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; private set; }
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+
+    }
+}
--- MarketPlace.Framework/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MarketPlace.Framework
-{
-    public class Class1
-    {
-    }
-}
