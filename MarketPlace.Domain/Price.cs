507e34fa2c04da81b7ef7f17e32b2630ec36839d
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-20 14:55:08 +0530

    Adds a price class.

---
+++ MarketPlace.Domain/Price.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MarketPlace.Domain
+{
+    public class Price : Money
+    {
+        public Price(decimal amount): base(amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Price cannot be negative", nameof(amount));
+        }
+    }
+}
