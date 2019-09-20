40bffdb6fce563dd9d162be47941b82c02d7ce4e
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-20 16:28:38 +0530

    Adds some more tests for testing the currency lookup domain service functionality.

--- MarketPlace.Tests/MoneyTest.cs
+++ MarketPlace.Tests/MoneyTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 using MarketPlace.Domain;
 
@@ -27,6 +28,73 @@ namespace MarketPlace.Tests
             var bankNote = Money.FromDecimal(5, "EUR", lookup);
 
             Assert.Equal(bankNote, coin1 + coin2 + coin3);
+        }
+
+
+        [Fact]
+        public void Two_of_same_amount_but_different_Currencies_should_not_be_equal()
+        {
+            var firstAmount = Money.FromDecimal(5, "EUR", lookup);
+            var secondAmount = Money.FromDecimal(5, "USD", lookup);
+
+            Assert.NotEqual(firstAmount, secondAmount);
+        }
+
+
+        [Fact]
+        public void FromString_and_FromDecimal_should_be_equal()
+        {
+            var firstAmount = Money.FromDecimal(5, "EUR", lookup);
+            var secondAmount = Money.FromString("5.00", "EUR", lookup);
+
+            Assert.Equal(firstAmount, secondAmount);
+        }
+
+
+        [Fact]
+        public void Unused_currency_should_not_be_allowed()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                Money.FromDecimal(100, "INR", lookup)
+            );
+        }
+
+        [Fact]
+        public void Unknown_currency_should_not_be_allowed()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                Money.FromDecimal(100, "WHAT?", lookup)
+            );
+        }
+
+        [Fact]
+        public void Throw_when_too_many_decimal_places()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                Money.FromDecimal(100.123m, "EUR", lookup)
+            );
+        }
+
+        [Fact]
+        public void Throws_on_adding_different_currencies()
+        {
+            var firstAmount = Money.FromDecimal(5, "USD", lookup);
+            var secondAmount = Money.FromDecimal(5, "EUR", lookup);
+
+            Assert.Throws<CurrencyMismatchException>(() =>
+                firstAmount + secondAmount
+            );
+        }
+
+        [Fact]
+        public void Throws_on_substracting_different_currencies()
+        {
+            var firstAmount = Money.FromDecimal(5, "USD", lookup);
+            var secondAmount = Money.FromDecimal(5, "EUR", lookup);
+
+            Assert.Throws<CurrencyMismatchException>(() =>
+                firstAmount - secondAmount
+            );
         }
     }
 }
