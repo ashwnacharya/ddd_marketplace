c579c92b13da969526ec465ba6b2931e2eb558ae
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-21 20:21:14 +0530

    Wires up the different classes that were created so far.

--- MarketPlace/Startup.cs
+++ MarketPlace/Startup.cs
@@ -9,12 +9,16 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
 using static System.Environment;
+using MarketPlace.Domain;
+using MarketPlace.Api;
+using Raven.Client;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
 
 namespace MarketPlace
 {
     public class Startup
     {
-
         public Startup(IHostingEnvironment environment, IConfiguration configuration)
         {
             Environment = environment;
@@ -28,6 +32,24 @@ namespace MarketPlace
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var store = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8080" },
+                Database = "Marketplace",
+                Conventions =
+                    {
+                        FindIdentityProperty = m => m.Name == "_databaseId"
+                    }
+            };
+            store.Conventions.RegisterAsyncIdConvention<ClassifiedAd>(
+                (dbName, entity) => Task.FromResult("ClassifiedAd/" + entity.Id.ToString()));
+            store.Initialize();
+
+            services.AddTransient(c => store.OpenAsyncSession());
+            services.AddSingleton<ICurrencyLookup, FixedCurrencyLookup>();
+            services.AddScoped<IClassifiedAdRepository, ClassifiedAdRepository>();
+            services.AddSingleton<ClassifiedAdsApplicationService>();
+            
             services.AddMvc();
             services.AddSwaggerGen(c =>
                 c.SwaggerDoc("v1",
