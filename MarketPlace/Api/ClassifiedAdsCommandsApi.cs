042329c202994fa7049663bf814ade82d90f4b8e
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-21 20:20:18 +0530

    Adds an Api controller.

--- MarketPlace/Api/ClassifiedAdsCommandsApi.cs
+++ MarketPlace/Api/ClassifiedAdsCommandsApi.cs
@@ -1,16 +1,57 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using static MarketPlace.Contracts.ClassifiedAds;
 
 namespace MarketPlace.Api
 {
     [Route("/ad")]
     public class ClassifiedAdsCommandsApi: Controller
     {
+        private readonly ClassifiedAdsApplicationService _applicationService;
+
+        public ClassifiedAdsCommandsApi(
+            ClassifiedAdsApplicationService applicationService) =>
+                _applicationService = applicationService;
+
         [HttpPost]
-        public async Task<ActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
+        public async Task<ActionResult> Post(V1.Create request)
         {
+            await _applicationService.Handle(request);
             return Ok();
         }
+
+        [Route("name")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SetTitle request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("text")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.UpdateText request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("price")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.UpdatePrice request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("publish")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.RequestToPublish request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
     }
 }
