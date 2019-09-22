9f186d44bab76fad76f782a1d0dd4149623f089c
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-22 16:43:37 +0530

    Adds the class to denote the picture being added.

---
+++ MarketPlace.Domain/Picture.cs
@@ -0,0 +1,38 @@
+using System;
+using MarketPlace.Framework;
+
+namespace MarketPlace.Domain
+{
+    public class PictureId: Value<PictureId>
+    {
+        public PictureId(Guid value) => Value = value;
+
+        public Guid Value { get; }
+    }
+
+    public class Picture: Entity<PictureId>
+    {
+        internal PictureSize Size {get; set; }
+        internal Uri Location { get; set; }
+        internal int Order {get; set; }
+
+        protected override void When(object @event)
+        {
+            switch (@event)
+            {
+                case Events.PictureAddedToAClassifiedAd e:
+                    Id = new PictureId(e.PictureId);
+                    Location = new Uri(e.Url);
+                    Size = new PictureSize
+                    {
+                        Height = e.Height,
+                        Width = e.Width
+                    };
+                    Order = e.Order;
+                    break;
+            }
+        }
+
+        public Picture(Action<object> applier) : base(applier) {}
+    }
+}
