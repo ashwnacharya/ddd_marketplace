c320130f1b0ce41b4f603f82489dad3512a4c8f2
ashwnacharya <b.ashwin.acharya@gmail.com>
2019-09-22 17:08:14 +0530

    Refactors the classified ad class slightly.

--- MarketPlace.Domain/ClassifiedAdId.cs
+++ MarketPlace.Domain/ClassifiedAdId.cs
@@ -5,19 +5,21 @@ namespace MarketPlace.Domain
 {
     public class ClassifiedAdId: Value<ClassifiedAdId>
     {
-        private readonly Guid _value;
+        public Guid Value { get; }
 
         public ClassifiedAdId(Guid value)
         {
             if (value == default)
                 throw new ArgumentNullException("Classified ad id cannot be empty", nameof(value));
 
-            _value = value;
+            Value = value;
         }
 
-        public static implicit operator Guid(ClassifiedAdId self) => self._value;
+        public static implicit operator Guid(ClassifiedAdId self) => self.Value;
 
         public static implicit operator ClassifiedAdId(string value) 
             => new ClassifiedAdId(Guid.Parse(value));
+
+        public override string ToString() => Value.ToString();
     }
 }
